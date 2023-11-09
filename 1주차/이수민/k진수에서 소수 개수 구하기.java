2f0ee23f02ccb56e487a9ffdb858b97492288f13
sum <58407737+leesuuuuumm@users.noreply.github.com>
2023-11-10 02:30:21 +0900

    [PRG] k진수에서 소수 개수 구하기

---
+++ 1주차/이수민/k진수에서 소수 개수 구하기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        if(n==1) return 0;
+       
+        String[] str = Integer.toString(n,k).split("0");
+        System.out.println(Arrays.toString(str));
+        a:for(int i=0;i<str.length;i++){
+            if(str[i].equals("") || str[i].equals("1")) continue;
+            long num = Long.parseLong(str[i]);
+             
+            for(int j=2;j<=Math.sqrt(num);j++){
+                if(num%j==0) continue a;
+            }
+            answer++;        
+        }
+        return answer;
+    }
+}
