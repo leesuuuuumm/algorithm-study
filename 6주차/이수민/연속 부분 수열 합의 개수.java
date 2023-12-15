8ac408924dbd67fd7641f5b3275c0ef5388753c5
sum <58407737+leesuuuuumm@users.noreply.github.com>
2023-12-16 01:09:03 +0900

    [PRG] 연속 부분 수열 합의 개수

---
+++ 6주차/이수민/연속 부분 수열 합의 개수.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] elements) {
+        Set<Integer> set = new HashSet<>();
+
+        int start = 1;
+        while(start < elements.length){
+            for (int i = 0; i < elements.length; i++){
+                int value = 0;
+                for (int j = i; j < i+start; j++){
+                    value += elements[j%elements.length];
+                }
+                set.add(value);
+            }
+            start++;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < elements.length; i++)
+            sum += elements[i];
+
+        set.add(sum);
+
+        return set.size();
+    }
+}
