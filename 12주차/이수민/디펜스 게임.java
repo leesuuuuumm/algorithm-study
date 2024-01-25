be809bcc1eeca9cb424ae23e379d8d99823fe069
sum <58407737+leesuuuuumm@users.noreply.github.com>
2024-01-26 01:12:03 +0900

    [PRG] 디펜스 게임

--- 12주차/이수민/디펜스 게임.java
+++ 12주차/이수민/디펜스 게임.java
@@ -1,5 +1,5 @@
 import java.util.*;
-
+ 
 class Solution {
     public int solution(int n, int k, int[] enemy) {
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
