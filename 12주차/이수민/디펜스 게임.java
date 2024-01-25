037702209006ac76f586e4e611c5e12372f7c34a
sum <58407737+leesuuuuumm@users.noreply.github.com>
2024-01-26 01:11:45 +0900

    [BOJ

---
+++ 12주차/이수민/디펜스 게임.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int k, int[] enemy) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        int nn = n;
+        int nk = k;
+        
+        for(int i=0;i<enemy.length;i++){
+            nn-=enemy[i];
+            pq.offer(enemy[i]);
+            if(nn<0){
+                while(!pq.isEmpty()){
+                    if(nk == 0) break;
+                
+                    nn+=  pq.poll();
+                    nk--;
+                    
+                    if(nn>0){
+                        break;
+                    }
+                }
+            }
+            if(nn<0){
+                return i;
+            }
+        }
+        
+        
+
+        return enemy.length; 
+    }
+}
