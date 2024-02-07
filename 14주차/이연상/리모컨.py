7752141afa7d1d423d2cd4a7afc17c8c005cae7f
YeonSang <leeys12181811@gmail.com>
2024-02-08 02:31:43 +0900

    [BOJ] 1107_리모컨

---
+++ 14주차/이연상/리모컨.py
@@ -0,0 +1,18 @@
+# [BOJ] 1107_리모컨
+
+target = int(input())
+ans = abs(100 - target)
+M = int(input())
+if M: 
+    broken = set(input().split())
+else:
+    broken = set()
+
+for num in range(1000001): 
+    for N in str(num):
+        if N in broken: 
+            break
+    else: 
+        ans = min(ans, len(str(num)) + abs(num - target))
+
+print(ans)
