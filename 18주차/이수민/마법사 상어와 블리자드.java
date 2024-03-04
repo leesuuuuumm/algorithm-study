8599af4eb198c9088d26c1018bc8c48ad8e4d95c
sum <58407737+leesuuuuumm@users.noreply.github.com>
2024-03-05 01:30:27 +0900

    [BOJ] 마법사 상어와 블리자드

--- 18주차/이수민/마법사 상어와 블리자드.java
+++ 18주차/이수민/마법사 상어와 블리자드.java
@@ -2,9 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
 	static int[][] map;
 	static int N, M;
+	static int[] marble;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,160 +13,170 @@ public class Main {
 		M = Integer.parseInt(st.nextToken());
 
 		map = new int[N][N];
-		que = new LinkedList<>();
-		tmp = new LinkedList<>();
-		sum = new int[4];
+		marble = new int[4];
 
 		for (int r = 0; r < N; r++) {
 			st = new StringTokenizer(br.readLine());
 			for (int c = 0; c < N; c++) {
 				map[r][c] = Integer.parseInt(st.nextToken());
+				if (map[r][c] == 0)
+					map[r][c] = -1;
 			}
 		}
 
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int dir = Integer.parseInt(st.nextToken()) - 1;
-			int S = Integer.parseInt(st.nextToken());
 
-			DoingBlizzard(dir, S);
-			inputMarbleInQueue();
-			removeMarble();
-			divideMarble();
-			makeMap();
+			int d = Integer.parseInt(st.nextToken()) - 1;
+			int s = Integer.parseInt(st.nextToken());
+			breakMables(d, s);
+
+			movingAndExplosion();
 
 		}
-		System.out.println(sum[1] + (2 * sum[2]) + (3 * sum[3]));
+		System.out.println(marble[1] + (2 * marble[2]) + (3 * marble[3]));
+
 	}
 
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };
 
-	private static void DoingBlizzard(int dir, int S) {
+	private static void breakMables(int nd, int ns) {
 		int nr = N / 2;
 		int nc = N / 2;
 
-		for (int s = 0; s < S; s++) {
-			nr += dr[dir];
-			nc += dc[dir];
+		for (int s = 0; s < ns; s++) {
+			nr += dr[nd];
+			nc += dc[nd];
 
 			if (!check(nr, nc))
 				break;
+
 			map[nr][nc] = 0;
+
 		}
 	}
 
-	static int[] sdir = { 2, 1, 3, 0 };
-	static Queue<Integer> que;
-	static Queue<Integer> tmp;
+	static int[] ddr = { 0, 1, 0, -1 };
+	static int[] ddc = { -1, 0, 1, 0 };
 
-	private static void inputMarbleInQueue() {
-		int sr = (N / 2);
-		int sc = (N / 2);
-		int sd = 0;
-		int cnt = 1;
+	private static void movingAndExplosion() {
+		Queue<Integer> que = new LinkedList<>();
+
+		int r = N / 2;
+		int c = N / 2;
+		int d = 0;
+		int k = 1;
+
 		e: while (true) {
+
 			for (int i = 0; i < 2; i++) {
-				for (int j = 0; j < cnt; j++) {
-					sr += dr[sdir[sd]];
-					sc += dc[sdir[sd]];
+				for (int j = 0; j < k; j++) {
+					r += ddr[d];
+					c += ddc[d];
 
-					if (!check(sr, sc))
+					if (r == -1 || c == -1 || map[r][c] == -1)
 						break e;
-					if (map[sr][sc] != 0) {
-						que.offer(map[sr][sc]);
-					}
+
+					if (map[r][c] == 0)
+						continue;
+
+					que.offer(map[r][c]);
 				}
-				sd = (sd + 1) % 4;
+
+				d++;
+				if (d == 4)
+					d = 0;
+
 			}
-			cnt++;
+			k++;
 		}
+		Queue<Integer> tmp;
 
-	}
+		while (true) {
+			boolean flag = false;
+			tmp = new LinkedList<>();
 
-	private static void removeMarble() {
-		boolean stop = false;
-		while (!stop) {
-			stop = true;
-			if (que.isEmpty()) {
-				break;
-			}
-			int size = que.size();
-			int cur = que.poll();
-			int count = 1;
-
-			for (int i = 0; i < size - 1; i++) {
-				if (cur == que.peek()) {
-					count++;
-				} else {
-					if (count > 3) {
-						sum[cur] += count;
-						stop = false;
-					} else {
-						for (int j = 0; j < count; j++) {
-							que.offer(cur);
+			while (!que.isEmpty()) {
+				int cur = que.poll();
+				if (que.isEmpty() || que.peek() != cur) {
+					tmp.offer(cur);
+				} else if (que.peek() == cur) {
+					int cnt = 1;
+					while (!que.isEmpty()) {
+						if (que.peek() == cur) {
+							cnt++;
+							que.poll();
+						} else {
+							break;
 						}
 					}
-					count = 1;
+					if (cnt < 4) {
+						for (int i = 0; i < cnt; i++) {
+							tmp.offer(cur);
+						}
+					}
+					if (cnt >= 4) {
+						marble[cur] += cnt;
+						flag = true;
+					}
 				}
-				cur = que.poll();
+
+			}
+			while (!tmp.isEmpty()) {
+				que.offer(tmp.poll());
 			}
 
-			if (count < 4) {
-				for (int j = 0; j < count; j++) {
-					que.offer(cur);
-				}
-			} else {
-				sum[cur] += count;
-				stop = false;
+			if (!flag)
+				break;
+
+		}
+
+		while (!que.isEmpty()) {
+			int cur = que.poll();
+			int cnt = 1;
+			for (int i = 0; i < que.size(); i++) {
+				if (cur == que.peek()) {
+					cnt++;
+					que.poll();
+				} else
+					break;
 			}
-		}
-	}
 
-	static int[] sum;
-	private static void divideMarble() {
-		int count = 1;
-		if (que.isEmpty()) {
-			return;
+			tmp.offer(cnt);
+			tmp.offer(cur);
 		}
-		int size = que.size();
-		int cur = que.poll();
-		for (int i = 0; i < size - 1; i++) {
-			if (cur == que.peek()) {
-				count++;
-			} else {
-				que.offer(count);
-				que.offer(cur);
 
-				count = 1;
-			}
-			cur = que.poll();
+		r = N / 2;
+		c = N / 2;
+		d = 0;
+		k = 1;
+
+		for (int i = 0; i < N; i++) {
+			Arrays.fill(map[i], -1);
 		}
-		que.offer(count);
-		que.offer(cur);
-	}
 
-	private static void makeMap() {
-		int sr = (N / 2);
-		int sc = (N / 2);
-		int sd = 0;
-		int cnt = 1;
-		map = new int[N][N];
 		e: while (true) {
-			for (int i = 0; i < 2; i++) {
-				for (int j = 0; j < cnt; j++) {
-					sr += dr[sdir[sd]];
-					sc += dc[sdir[sd]];
 
-					if (que.isEmpty() || !check(sr, sc))
+			for (int i = 0; i < 2; i++) {
+				for (int j = 0; j < k; j++) {
+					r += ddr[d];
+					c += ddc[d];
+
+					if (r == -1 || c == -1 || tmp.isEmpty())
 						break e;
-					map[sr][sc] = que.poll();
+
+					map[r][c] = tmp.poll();
 				}
-				sd = (sd + 1) % 4;
+
+				d++;
+				if (d == 4)
+					d = 0;
+
 			}
-			cnt++;
+			k++;
 		}
-		que.clear();
+
 	}
 
 	private static boolean check(int nr, int nc) {
