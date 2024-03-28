f4e0657a686e9efa156cc0220711e04e6f709ae1
sum <58407737+leesuuuuumm@users.noreply.github.com>
2024-03-29 00:26:49 +0900

    [BOJ] LCS

--- 21주차/이수민/LCS.java
+++ 21주차/이수민/LCS.java
@@ -1,7 +1,7 @@
 import java.io.*;
 
 public class Main { 
-
+ 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		char[] s2 = br.readLine().toCharArray();
