68ac8c8c03b19ef127a825e743f78a9b4e8b08fc
Yeonsang <95534831+leeys1218@users.noreply.github.com>
2024-07-08 23:19:22 +0900

    [BOJ] 최소 회읫실 개수

---
+++ 36주차/이연상/최소 회의실 개수.java
@@ -0,0 +1,50 @@
+// [BOJ] 최소 회읫실 개수
+
+public class P_19598 {
+    static int N;
+    static PriorityQueue<Time> pq;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        pq = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            pq.add(new Time(Integer.parseInt(st.nextToken()), true)); // 시작시간
+            pq.add(new Time(Integer.parseInt(st.nextToken()), false)); // 종료시간
+        }
+
+        int cnt = 0; // 회의실 개수
+        int answer = 0; // 최대값
+
+        while (!pq.isEmpty()) {
+            Time t = pq.poll();
+
+            if (t.isStart) {
+                cnt++;
+                answer = Math.max(cnt, answer);
+            }
+            else {
+                cnt--;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    static class Time implements Comparable<Time>{
+        int time;
+        boolean isStart;
+
+        public Time(int time, boolean isStart) {
+            this.time = time;
+            this.isStart = isStart;
+        }
+
+        @Override
+        public int compareTo(Time o) {
+            return this.time - o.time;
+        }
+    }
+}
