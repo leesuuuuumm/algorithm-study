3b314b89098ddccc85ab5e51a7c777bc9b6b8148
Yeonsang <95534831+leeys1218@users.noreply.github.com>
2024-08-02 22:50:42 +0900

    [PRG] 예상 대진표

---
+++ 39주차/이연상/예상 대진표.java
@@ -0,0 +1,18 @@
+// [PRG] 예상 대진표
+
+import java.util.*;
+
+class Solution
+{
+    
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;  
+        while (a != b) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            answer++;
+        }
+        return answer;
+    }
+}
