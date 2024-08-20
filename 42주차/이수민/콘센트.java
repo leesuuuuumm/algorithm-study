307102a86236e3aad8bf5f54ee52f37f69a46ad7
sum <58407737+leesuuuuumm@users.noreply.github.com>
2024-08-21 00:31:23 +0900

    [BOJ] 23843_콘센트

--- 42주차/이수민/콘센트.java
+++ 42주차/이수민/콘센트.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException { 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
