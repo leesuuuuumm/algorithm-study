a37b319ea7317ab0849509f06e20ef04cf027fea
sum <58407737+leesuuuuumm@users.noreply.github.com>
2024-08-23 02:46:38 +0900

    [BOJ] 귤 고르기

---
+++ 41주차/이수민/귤 고르기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<tangerine.length;i++){
+            map.put(tangerine[i],map.getOrDefault(tangerine[i],0)+1);
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for(Integer i: map.keySet()){
+            list.add(map.get(i));
+        }
+        Collections.sort(list, Collections.reverseOrder());
+        
+        for(Integer i: list){
+            answer++;
+            if(k<=i){
+                break;
+            }
+            k-=i;
+        }
+        return answer;
+    }
+}
