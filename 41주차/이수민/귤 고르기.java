71486dd02ce26d69b0cb8b08a0a844aaa7913177
sum <58407737+leesuuuuumm@users.noreply.github.com>
2024-08-23 02:47:12 +0900

    [PRG[ 귤고르기

--- 41주차/이수민/귤 고르기.java
+++ 41주차/이수민/귤 고르기.java
@@ -3,7 +3,7 @@ class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
         
-        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>(); 
         for(int i=0;i<tangerine.length;i++){
             map.put(tangerine[i],map.getOrDefault(tangerine[i],0)+1);
         }
