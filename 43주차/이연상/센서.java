c279c93f6c82c19dccd31479b4e8dc4a10373021
Yeonsang <95534831+leeys1218@users.noreply.github.com>
2024-08-27 00:21:43 +0900

    [BOJ] 센서

---
+++ 43주차/이연상/센서.java
@@ -0,0 +1,32 @@
+// [BOJ] 센서
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt(); 
+		if(k >= n) {
+			System.out.println(0);
+			return;
+		}
+		int sum = 0;
+		
+		int[] sensorArr = new int[n];
+		for(int i = 0; i < n; i++)
+			sensorArr[i] = sc.nextInt();
+		
+		Arrays.sort(sensorArr);
+		
+		Integer[] diffArr = new Integer[n-1];
+		for(int i = 0; i < n-1; i++)
+			diffArr[i] = sensorArr[i+1] - sensorArr[i];
+		
+		Arrays.sort(diffArr, Collections.reverseOrder());
+		for(int i = k-1; i < n-1; i++) {
+			sum += diffArr[i];
+		}
+		System.out.println(sum);
+	}
+}
