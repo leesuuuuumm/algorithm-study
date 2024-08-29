8d596e03bfb2db33051b08a744126e253f7a6a79
Yeonsang <95534831+leeys1218@users.noreply.github.com>
2024-08-29 11:32:34 +0900

    [PRG] 의상

---
+++ 43주차/이연상/의상.java
@@ -0,0 +1,20 @@
+// [PRG] 의상
+
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String[] ct : clothes) {
+            map.put(ct[1], map.getOrDefault(ct[1], 0) + 1);
+        }
+        
+        for(String key : map.keySet()) {
+            answer *= map.get(key) + 1;
+        }
+        
+        return answer - 1;
+    }
+}
